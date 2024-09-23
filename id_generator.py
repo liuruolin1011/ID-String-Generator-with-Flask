751af0b5745235d09ed47ecd74a06421227099e6
Ruolin Liu <112987077+liuruolin1011@users.noreply.github.com>
2024-09-23 19:01:47 -0400

    Update id_generator.py

--- id_generator.py
+++ id_generator.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/generate', methods = ['POST'])
 def generate():
-  numbers = request.form['numbers']
-  numbers_list = numbers.split()
-  formatted_numbers = [f'"{numbers}"' for numbers in numbers_list]
-  result = f'( [Alert ID] Is {" | ".join(formatted_numbers)} )'
-  return render_template('index.html', result=result)
+    numbers = request.form['numbers']
+    numbers_list = numbers.split()
+    formatted_numbers = [f'{{numbers}}' for numbers in numbers_list]
+    result = f'[Alert ID] Is {" | ".join(formatted_numbers)}'
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True, host='00.000.000', port=5013)
